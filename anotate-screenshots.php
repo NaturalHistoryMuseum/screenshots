c499e61fc15b37a256299313d35530f80f2008f1
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-01-05 10:34:56 +0000

    Quick tweak.
    git-svn-id: http://svn.scratchpads.eu/svn/screenshots@2 268ec2f5-2547-4eb8-bf82-3d67d37779f7

--- anotate-screenshots.php
+++ anotate-screenshots.php
@@ -2,7 +2,10 @@
 $convert_string = "convert -background '#0008' -fill white -gravity west -size 1024x500 caption:\"%s\" +size %s +swap -gravity south -composite %s";
 // Get a list of all the files in the directory, and filter HARD!
 $files = scandir('.');
-$databasedetails = parse_ini_file('/var/www/drupal_db_passwords',true);
+// Get the details from dev.scratchpads.eu
+$databasedetails = parse_ini_file('/etc/drupal/6/drupal_db_passwords',true);
+print_r($databasedetails);
+exit;
 foreach($files as $file){
   if(strpos($file,'.jpg')>0){
     // $file is the start need to connect to a database now, and do some stuff
