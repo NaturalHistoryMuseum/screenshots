fc27701144de1dfa4f468405aae9acdc0c4c0e80
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-01-05 11:06:23 +0000

    Tweaking
    git-svn-id: http://svn.scratchpads.eu/svn/screenshots@4 268ec2f5-2547-4eb8-bf82-3d67d37779f7

--- anotate-screenshots.php
+++ anotate-screenshots.php
@@ -6,27 +6,24 @@ $files = scandir('.');
 $databasedetails = parse_ini_file('/etc/drupal/6/drupal_db_passwords',true);
 mysql_connect("localhost", $databasedetails['devscratchpadseu']['user'], $databasedetails['devscratchpadseu']['password'], TRUE, 2);
 mysql_select_db('devscratchpadseu');
-$result = mysql_query("SELECT value FROM variable WHERE name = 'scratchpad_sites_list'");
-while($row = mysql_fetch_array($result)){
-  print_r($row);
-}
-exit;
-foreach($files as $file){
-  if(strpos($file,'.jpg')>0){
-    // $file is the start need to connect to a database now, and do some stuff
-    $domain = str_replace('-','',array_shift(explode(".", $file)));
-    if($domain =="pad"){
-      $domain = explode(".",$file);
-      $domain = str_replace('-','',$domain[1]);
-    }
-    $nodes = array_pop(mysql_fetch_array(mysql_query("SELECT COUNT(nid) AS nodes FROM node;")));
-    $users = array_pop(mysql_fetch_array(mysql_query("SELECT COUNT(uid) AS users FROM users")));
-    $views = array_pop(mysql_fetch_array(mysql_query("SELECT SUM(totalcount) AS totalcount FROM node_counter;")));
-    $caption = ' Pages: '.$nodes.'\n Users: '.$users.'\n Views: '.$views;
-    $file_parts = explode('.',$file);
-    array_pop($file_parts);
-    array_push($file_parts,'annotated.jpg');
-    $filenew = implode('.',$file_parts);
-    echo sprintf($convert_string,$caption,$file,$filenew)."\n";
+$sites = unserialize(array_pop(db_fetch_array(mysql_query("SELECT value FROM variable WHERE name = 'scratchpad_sites_list'"))));
+foreach($sites as $url => $site_details){
+  echo "$url\n";
+  /*
+  // $file is the start need to connect to a database now, and do some stuff
+  $domain = str_replace('-','',array_shift(explode(".", $file)));
+  if($domain =="pad"){
+    $domain = explode(".",$file);
+    $domain = str_replace('-','',$domain[1]);
   }
+  $nodes = array_pop(mysql_fetch_array(mysql_query("SELECT COUNT(nid) AS nodes FROM node;")));
+  $users = array_pop(mysql_fetch_array(mysql_query("SELECT COUNT(uid) AS users FROM users")));
+  $views = array_pop(mysql_fetch_array(mysql_query("SELECT SUM(totalcount) AS totalcount FROM node_counter;")));
+  $caption = ' Pages: '.$nodes.'\n Users: '.$users.'\n Views: '.$views;
+  $file_parts = explode('.',$file);
+  array_pop($file_parts);
+  array_push($file_parts,'annotated.jpg');
+  $filenew = implode('.',$file_parts);
+  echo sprintf($convert_string,$caption,$file,$filenew)."\n";
+*/
 }
