5d957d6b521bac5306d8b7e2892f9f12d1cf5f70
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-04-26 08:31:43 +0000

    Added extra and tweaked files for the species of the week rubbish.
    git-svn-id: http://svn.scratchpads.eu/svn/screenshots@26 268ec2f5-2547-4eb8-bf82-3d67d37779f7

--- anotate-screenshots.php
+++ anotate-screenshots.php
@@ -9,6 +9,14 @@ mysqli_real_connect($connection, "localhost", $databasedetails['devscratchpadseu
 mysqli_query($connection, 'SET NAMES "utf8"');
 $sites = unserialize(array_pop(mysqli_fetch_array(mysqli_query($connection, "SELECT value FROM variable WHERE name = 'scratchpad_sites_list'", MYSQLI_USE_RESULT))));
 foreach($sites as $url => $site_details){
-  $caption = ' Pages: '.array_sum($site_details['nodes']).'\n Users: '.$site_details['users']['total'].'\n Views: '.$site_details['views'];
-  echo sprintf($convert_string,$caption,"$url.jpg","$url.annotated.jpg")."\n";
+  $site_details = $sites[$url];
+  if(is_array($site_details)){
+    if(is_array($site_details['nodes'])){
+      $nodes = array_sum($site_details['nodes']);
+    } else {
+      $nodes = 1;
+    }
+    $caption = ' Pages: '.$nodes.'\n Users: '.$site_details['users']['total'].'\n Views: '.$site_details['views'];
+    echo sprintf($convert_string,$caption,"$url.jpg","$url.annotated.jpg")."\n";
+  }
 }
